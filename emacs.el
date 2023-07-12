5d16e0c6e7e0f7ae00a42a2978596be50ec5dcdd
Jackson Brough <jacksontbrough@gmail.com>
2023-07-12 01:29:07 -0600

    Just use gnome, there are way too many rofi clones to choose from

--- emacs.el
+++ emacs.el
@@ -5,9 +5,45 @@
 (setq use-package-always-ensure t)
 (eval-when-compile (require 'use-package))
 
+;; TODO: Actually understand these from long ago and hopefully get rid of most of it
+;; TODO: Make path implicit
+(setq local-directory (expand-file-name "~/home/local/data/emacs/"))
+(setq backup-directory (concat local-directory "backups/"))
+(setq auto-save-directory (concat local-directory "auto-saves/"))
+(setq backup-directory-alist '(("*" . ,backup-directory)))
+(setq backup-inhibited t)
+(setq auto-save-file-name-transforms `((".*" ,auto-save-directory t)))
+(setq auto-save-list-file-prefix auto-save-directory)
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
+(setq vc-make-backup-files t)
+;; TODO: Ideally just don't have one
+(setq custom-file (concat user-emacs-directory "custom.el"))
+
 (add-to-list 'default-frame-alist
 	     '(font . "JetBrainsMono 12"))
+(setq visible-bell t)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
 
 (use-package evil
+  :custom
+  (evil-undo-system 'undo-redo)
   :config
   (evil-mode 1))
+
+(use-package which-key
+  :config (which-key-mode 1))
+
+(use-package company
+  :custom
+  (company-idle-delay 0.1)
+  :bind
+  (:map company-active-map
+	("C-n" . company-select-next)
+	("C-p" . company-select-previous))
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package nix-mode
+  :mode "\\.nix\\'")
--- flake.nix
+++ flake.nix
@@ -7,20 +7,19 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # hyprland.url = "github:hyprwm/Hyprland";
-
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
   outputs = { nixpkgs, home-manager, emacs-overlay, ... }:
     let
       inherit (nixpkgs) lib;
+      # TODO: Use flake-utils to make this generic? (For kenobi)
       system = "x86_64-linux";
       # TODO: I'm confused, nixpkgs doesn't look like a function in it's flake.nix
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        overlays = [emacs-overlay.overlays.emacs emacs-overlay.overlays.package];
+        overlays = with emacs-overlay.overlays; [ emacs package ];
       };
     in
     {
@@ -43,8 +42,8 @@
               # TODO: What is this needed for?
               security.polkit.enable = true;
 
-	      # TODO:
-	      /*
+	            # TODO: Gnome didn't like pipewire
+	            /*
               security.rtkit.enable = true;
               services.pipewire = {
                 enable = true;
@@ -65,33 +64,51 @@
                   }
                 '';
               };
-	      */
+	            */
 
               services.xserver = {
                 enable = true;
                 displayManager.gdm.enable = true;
                 displayManager.gdm.wayland = true;
                 desktopManager.gnome.enable = true;
-		videoDrivers = [ "nvidia" ];
+		            videoDrivers = [ "nvidia" ];
               };
-	      environment.gnome.excludePackages = (with pkgs; [
-	        gnome-photos
-		gnome-tour
-	      ]) ++ (with pkgs.gnome; [
-	        cheese
-  		gnome-music
-  		gnome-terminal
-  		gedit
-  		epiphany
-  		geary
-  		evince
-  		gnome-characters
-  		totem
-  		tali
-  		iagno
-  		hitori
-  		atomix
-	      ]);
+	            environment.gnome.excludePackages = (with pkgs; [
+	              gnome-photos
+		            gnome-tour
+	            ]) ++ (with pkgs.gnome; [
+	              cheese
+  		          atomix
+  		          epiphany
+  		          evince
+  		          geary
+  		          gedit # @Conman
+  		          gnome-characters
+  		          gnome-music
+  		          hitori
+  		          iagno
+  		          tali
+  		          totem
+                gnome-calculator
+                gnome-calendar
+                gnome-clocks
+                gnome-contacts
+                gnome-maps
+                gnome-weather
+                # gnome-disk-image-mounter
+                # gnome-disks
+                # gnome-extensions
+                # gnome-extensions-app
+                # gnome-logs
+                # gnome-system-monitor
+                simple-scan
+	            ]) ++ (with pkgs.gnome.apps; [
+                # TODO: Figure how to remove these
+                # gnome-connections
+                # gnome-help
+                # gnome-text-editor
+                # gnome-thumbnail-font
+              ]);
               hardware.opengl = {
                 enable = true;
                 driSupport = true;
@@ -124,14 +141,12 @@
 
               nixpkgs.config.allowUnfree = true;
 
-              # Prevent a world of pain where the nix tool has you fetching rocks 
-              # for a long time until you finally discover that it wants git but refuses 
-              # to tell you that.
+              # Prevent a world of pain where nix 2.4 has you fetching rocks for
+              # a long time until you finally discover that it wanted git but
+              # refused to tell you.
               environment.systemPackages = with pkgs; [ curl git neovim ];
 
               programs.fish.enable = true;
-              # TODO: Does this actually help
-              # programs.light.enable = true;
 
               services.openssh = {
                 enable = true;
@@ -163,6 +178,7 @@
             let
               # TODO: Other duplicated stuff goes here
               userName = "jackson";
+              email = "jacksontbrough@gmail.com";
               emacs = (pkgs.emacsWithPackagesFromUsePackage {
                 config = ./emacs.el;
                 defaultInitFile = true;
@@ -171,19 +187,11 @@
               });
             in {
               home.username = userName;
-              home.homeDirectory = "/home/jackson";
+              home.homeDirectory = "/home/${userName}";
 
               home.stateVersion = "23.05";
 
-              # home.sessionVariables = {
-              #   # Magic wayland incantations
-              #   LIBVA_DRIVER_NAME = "nvidia";
-              #   XDG_SESSION_TYPE = "wayland";
-              #   GBM_BACKEND = "nvidia-drm";
-              #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-              #   WLR_NO_HARDWARE_CURSORS = "1";
-              #   MOZ_ENABLE_WAYLAND = "1";
-              # };
+              programs.home-manager.enable = true;
 
               home.packages = with pkgs; [
                 ripgrep
@@ -196,26 +204,20 @@
 
                 gopass
                 pinentry-gnome
-                # swaybg
 
                 spotify
               ];
 
               fonts.fontconfig.enable = true;
 
-              programs.home-manager.enable = true;
-
               programs.fish.enable = true;
 
               programs.git = {
                 enable = true;
-                userName = "Jackson Brough";
-                userEmail = "jacksontbrough@gmail.com";
+                userEmail = email;
               };
 
-              programs.ssh = {
-                enable = true;
-              };
+              programs.ssh.enable = true;
               services.ssh-agent.enable = true;
 
               programs.gpg = {
@@ -225,9 +227,6 @@
               services.gpg-agent = {
                 enable = true;
                 pinentryFlavor = "gnome3";
-                # TODO: Surely there's something I don't understand about derivations and 
-                # there's a way to do this without the hack
-                # extraConfig = "pinentry-program ${pkgs.pinentry-rofi.out}/bin/pinentry-rofi";
               };
 
               xdg.configFile.gopass = { 
@@ -240,161 +239,6 @@
                 '';
               };
 
-              # TODO: Download nasa wallpaper and output in some pictures directory
-	      /*
-              wayland.windowManager.hyprland = {
-                enable = true;
-                extraConfig = ''
-                exec-once = waybar
-                # TODO: Remove
-                exec-once = swaybg -i ~/cosmic-cliffs.png
-
-                windowrule = workspace 1 silent, emacs
-                exec-once = emacsclient -c
-
-                monitor = ,preferred,auto,auto
-
-                env = XCURSOR_SIZE,24
-                input {
-                    kb_layout = us
-                    kb_variant =
-                    kb_model =
-                    kb_options =
-                    kb_rules =
-                
-                    follow_mouse = 1
-                
-                    touchpad {
-                        natural_scroll = yes
-                    }
-
-                    sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-                }
-
-                general {
-                    gaps_in = 5
-                    gaps_out = 20
-                    border_size = 2
-                    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-                    col.inactive_border = rgba(595959aa)
-
-                    layout = dwindle
-                }
-
-                decoration {
-                    rounding = 10
-                    blur = yes
-                    blur_size = 3
-                    blur_passes = 1
-                    blur_new_optimizations = on
-
-                    drop_shadow = yes
-                    shadow_range = 4
-                    shadow_render_power = 3
-                    col.shadow = rgba(1a1a1aee)
-                }
-
-                animations {
-                    enabled = yes
-
-                    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
-                    animation = windows, 1, 7, myBezier
-                    animation = windowsOut, 1, 7, default, popin 80%
-                    animation = border, 1, 10, default
-                    animation = borderangle, 1, 8, default
-                    animation = fade, 1, 7, default
-                    animation = workspaces, 1, 6, default
-                }
-
-                dwindle {
-                    pseudotile = yes
-                    preserve_split = yes
-                    no_gaps_when_only = yes
-                }
-
-                master {
-                    new_is_master = true
-                }
-
-                gestures {
-                    workspace_swipe = on
-                }
-
-                $modifier = SUPER
-
-                bind = $modifier SHIFT, Q, exit,
-                bind = $modifier, Q, killactive,
-                bind = $modifier, Return, exec, alacritty
-                bind = $modifier, D, exec, rofi -show run
-                bind = $modifier, V, togglefloating
-
-                bind = $modifier, H, movefocus, l
-                bind = $modifier, J, movefocus, r
-                bind = $modifier, K, movefocus, u
-                bind = $modifier, L, movefocus, d
-                
-                bind = $modifier, 1, workspace, 1
-                bind = $modifier, 2, workspace, 2
-                bind = $modifier, 3, workspace, 3
-                bind = $modifier, 4, workspace, 4
-                bind = $modifier, 5, workspace, 5
-                bind = $modifier, 6, workspace, 6
-                bind = $modifier, 7, workspace, 7
-                bind = $modifier, 8, workspace, 8
-                bind = $modifier, 9, workspace, 9  
-                bind = $modifier, 0, workspace, 10
-                
-                bind = $modifier SHIFT, 1, movetoworkspace, 1
-                bind = $modifier SHIFT, 2, movetoworkspace, 2
-                bind = $modifier SHIFT, 3, movetoworkspace, 3
-                bind = $modifier SHIFT, 4, movetoworkspace, 4
-                bind = $modifier SHIFT, 5, movetoworkspace, 5
-                bind = $modifier SHIFT, 6, movetoworkspace, 6
-                bind = $modifier SHIFT, 7, movetoworkspace, 7
-                bind = $modifier SHIFT, 8, movetoworkspace, 8
-                bind = $modifier SHIFT, 9, movetoworkspace, 9
-                bind = $modifier SHIFT, 0, movetoworkspace, 10
-
-                # TODO: Can we get rid of 272 and 273?
-                bind = $modifier, mouse:272, movewindow
-                bind = $modifier, mouse:273, resizewindowpixel
-
-                bindsym MonBrightnessDown exec light -U 10
-                bindsym MonBrightnessUp exec light -A 10
-
-                bindsym AudioRaiseVolume exec 'wpctl set-volume @DEFAULT_SINK@ 5%+'
-                bindsym AudioLowerVolume exec 'wpctl set-volume @DEFAULT_SINK@ 5%-'
-                bindsym AudioMute exec 'wpctl set-mute @DEFAULT_SINK@ toggle'
-                '';
-              };
-	      */
-
-              /*
-              programs.waybar = {
-                enable = true;
-                settings = {
-                  mainBar = {
-                    layer = "top";
-                    position = "top";
-                    height = 30;
-                    spacing = 4;
-                    modules-left = ["hyprland/workspaces"];
-                    modules-right = ["bluetooth" "network" "pulseaudio" "battery" "clock"];
-                  };
-                };
-              };
-	      */
-
-	      /*
-              programs.rofi = {
-                enable = true;
-                package = pkgs.rofi-wayland;
-                pass.enable = true;
-                pass.stores = [ "/home/jackson/repositories/passwords" ];
-              };
-	      */
-
               programs.alacritty = {
                 enable = true;
                 settings = {
@@ -405,7 +249,6 @@
 
               programs.firefox = {
                 enable = true;
-		# TODO: true
                 enableGnomeExtensions = false;
               };
 
