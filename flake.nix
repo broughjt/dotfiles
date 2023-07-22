e7550dbda24b732faa28de2dbd435ba3d4071fc6
Jackson Brough <jacksontbrough@gmail.com>
2023-07-22 17:16:37 -0600

    Add caches

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
 {
   description = "Are these your configuration files, Larry?";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
