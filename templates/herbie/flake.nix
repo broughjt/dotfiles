454a9411f8ee028e2cefa7306a5d59a90b0c61d1
Jackson Brough <jacksontbrough@gmail.com>
2023-08-14 13:39:38 -0600

    Add herbie template

---
+++ templates/herbie/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Herbie virtual environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay ];
+          };
+        in
+        with pkgs;
+        {
+          # TODO: Figure out how to not even potentially touch the local ~/.local/racket and install packages locally
+          devShells.default = mkShell {
+            buildInputs = [
+              rust-bin.stable.latest.default
+              rust-analyzer
+              racket-minimal
+              just
+            ];
+          };
+        }
+      );
+}
