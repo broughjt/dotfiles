a391510df651351274cec46e94af5e663dccae54
Jackson Brough <jacksontbrough@gmail.com>
2023-08-16 16:41:25 -0600

    agenix sadness

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,8 @@
 let
   jackson = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICv+kva6ORZ2Z9FZNi8ufzzYPQKzy1WvhAYDQt4kEiFU";
 
-  share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJDyIr/FSz1cJdcoW69R+NrWzwGK/+3gJpqD1t8L2zE";
+  share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQmoxow4kKpKqJos6mwz17YdeIxCRL7SzUlS9XUfcHQ";
 in
 {
-  "share1-auth-key1.age".publicKeys = [ share1 ];
+  "share1-auth-key1.age".publicKeys = [ jackson share1 ];
 }
