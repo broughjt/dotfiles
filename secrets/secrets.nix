3f074f1e70a42b955e38df73ac81bd1ee42f0778
Jackson Brough <jacksontbrough@gmail.com>
2023-08-16 21:16:28 -0600

    Wrong secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,5 @@
 let
   jackson = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICv+kva6ORZ2Z9FZNi8ufzzYPQKzy1WvhAYDQt4kEiFU";
-
   share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8ZBNR7Lut68bq2jubQpvC4ExQos2pMfNjmRj828Wu8";
 in
 {
