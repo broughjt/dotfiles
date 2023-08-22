40009e2ba3999e8de0426e8ff16a03b6eb82b241
Jackson Brough <jacksontbrough@gmail.com>
2023-08-22 10:53:21 -0600

    Fix herbie's template

--- templates/herbie/flake.nix
+++ templates/herbie/flake.nix
@@ -16,12 +16,12 @@
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ rust-overlay ];
+            overlays = [ rust-overlay.overlays.default ];
           };
         in
         with pkgs;
         {
-          # TODO: Figure out how to not even potentially touch the local ~/.local/racket and install packages locally
+          # TODO: Figure out how to not even touch the local ~/.local/racket and install packages locally
           devShells.default = mkShell {
             buildInputs = [
               rust-bin.stable.latest.default
