dee0f52f12445808cc6a4115083c8e24f60335d0
Jackson Brough <jacksontbrough@gmail.com>
2023-08-25 16:29:59 -0600

    Change ssh keys again ugh

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   jackson = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICv+kva6ORZ2Z9FZNi8ufzzYPQKzy1WvhAYDQt4kEiFU";
-  share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8ZBNR7Lut68bq2jubQpvC4ExQos2pMfNjmRj828Wu8";
+  share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYfJm9tI/Tn8cJfpEC1MOrJbCk0kVL+FHpYxcEXuJYe";
 in
 {
   "share1-auth-key1.age".publicKeys = [ jackson share1 ];
