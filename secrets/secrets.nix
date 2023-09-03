ebb6b96eb02bbc557bd24ee7ce28fde14a93911b
Jackson Brough <jacksontbrough@gmail.com>
2023-09-03 01:55:09 -0600

    Try again

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,6 @@
 let
   kenobi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBndIK51b/o6aSjuTdoa8emnpCRg0s5y68oXAFR66D4/ jacksontbrough@gmail.com";
-  # linode1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILF66MeoetuuV9j4kMEYgsCNcE1b4BOGxX8gIhc+oVUA root@linode1";
-  # linode1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINT4VbFNg0rrV/5eBQPLSGSwKstkCPdt+/iptFDQ8NG4 root@linode1";
-  linode1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlN40Eg1sbl3c1C5Jw1BtTtKSbX8CMvpMmszGJyxH2j root@nixos";
+  linode1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJW41FXloY8w4tjYHbnfsopvpc8USCoSDsRKlInYbJkh root@linode1";
 in
 {
   "share1-auth-key1.age".publicKeys = [ kenobi ];
