7d12ec6b84b41fb059af6ec1e993bca7e0e787f6
Jackson Brough <jacksontbrough@gmail.com>
2023-10-10 13:33:50 -0600

    Wifi sadness

--- flake.nix
+++ flake.nix
@@ -967,6 +967,13 @@
         ];
       };
       packages.aarch64-linux.raspberryPi4Image = nixosConfigurations.raspberryPi4Image.config.system.build.sdImage;
+      nixosConfigurations.share1Image = nixpkgs.lib.nixosSystem {
+        modules = [
+          nixosModules.share1
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+        ];
+      };
+      packages.aarch64-linux.share1Image = nixosConfigurations.raspberryPi4Image.config.system.build.sdImage;
       formatter = nixpkgs.lib.genAttrs [ "x86_64-darwin" "x86_64-linux" "aarch64-linux" ] (system: {
         system = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
       });
