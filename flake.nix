6a204028d82affaff3932fbcf4255d3096e0604f
Jackson Brough <jacksontbrough@gmail.com>
2023-10-09 20:05:16 -0600

    Magic incantations

--- flake.nix
+++ flake.nix
@@ -127,7 +127,6 @@
             ];
         
             boot = {
-              kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
               initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
               loader = {
                 grub.enable = false;
