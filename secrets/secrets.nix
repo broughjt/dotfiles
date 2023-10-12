f8547c62609cd31d2c67c3bfde74b5c871e10a22
Jackson Brough <jacksontbrough@gmail.com>
2023-10-11 19:37:35 -0600

    Yawp

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   kenobi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBndIK51b/o6aSjuTdoa8emnpCRg0s5y68oXAFR66D4/ jacksontbrough@gmail.com";
-  share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPALAd6ycOmMNNcoN52p6LyGctpW//2jrSDisc6I7qB6 root@share1";
+  share1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpnGMEUElcwgnuHpBXQa4xotZrRdT6VC/7b9n5TykXZ root@share1";
 in
 {
   "share1-auth-key1.age".publicKeys = [ kenobi share1 ];
