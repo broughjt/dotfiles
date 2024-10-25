81ca9b55d40cd0bcb4cd8a2e232c2c2093e7c046
Jackson Brough <jacksontbrough@gmail.com>
2024-10-25 12:19:40 -0600

    Make Verilog less annoying

--- emacs.el
+++ emacs.el
@@ -228,6 +228,20 @@
   :hook
   ((haskell-mode . eglot-ensure)))
 
+(setq verilog-indent-level 4)
+(setq verilog-case-indent 4)
+(setq verilog-cexp-indent 4)
+(setq verilog-indent-level-behavioral 4)
+(setq verilog-indent-level-declaration 4)
+(setq verilog-indent-level-module 4)
+(setq verilog-indent-level-module 4)
+(setq verilog-align-ifelse t)
+(setq verilog-auto-delete-trailing-whitespace t)
+(setq verilog-auto-newline nil)
+(setq verilog-auto-lineup nil)
+(setq verilog-highlight-grouping-keywords t)
+(setq verilog-highlight-modules t)
+
 (use-package magit)
 
 (use-package nix-mode
