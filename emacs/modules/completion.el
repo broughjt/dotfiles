085ddd642c2325e3cfb5bdc02cbcbe6676724d66
Jackson Brough <jacksontbrough@gmail.com>
2026-02-25 18:56:26 -0700

    Add affe, whoa

--- emacs/modules/completion.el
+++ emacs/modules/completion.el
@@ -12,13 +12,49 @@
          ("C-x p b" . consult-project-buffer)
          ("M-g i" . consult-imenu)
          ("M-g I" . consult-imenu-multi)
-         ("M-s d" . consult-find)
+         ;; ("M-s d" . consult-find)
          ("M-s g" . consult-ripgrep)))
 
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(defvar rg-ignore-flags
+  "-g \"!*.mp3\" -g \"!*.jpg\" -g \"!*.JPG\" -g \"!*.jpeg\" -g \"!*.png\" \
+  -g \"!*.mkv\" -g \"!*.mp4\" -g \"!*.avi\" -g \"!*.zip\" -g \"!*.ddl\" \
+  -g \"!*.ods\" -g \"!*.xlsx\" -g \"!*.m3u\" -g \"!*.url\" -g \"!*.aac\" \
+  -g \"!*.mpc\" -g \"!*.sql\" -g \"!*.ydb\" -g \"!dist/\" \
+  -g \"!.git/\" -g \"!git/*\" -g \"!node_modules/\" -g \"!*cache/\" \
+  -g \"!.cache\" -g \"!vendor/\" \
+  -g \"!.pki/\" -g \"!.local/share/*/\" \
+  -g \"!coverage\" -g \"!build/\" -g \"!var/\" -g \"!npm/\" \
+  -g \"!Library/\" -g \"!.DS_Store\" -g \"!.stfolder\""
+  "Exclusion flags for usage with ripgrep commands.")
+(defvar rg-find-files-command
+  (format "rg -L --ignore --hidden --files --color=never %s" rg-ignore-flags)
+  "Command for finding files with ripgrep.")
+(defvar rg-find-directories-command
+  (format "rg-dir -L --ignore --hidden --color=never %s" rg-ignore-flags)
+  "Command for finding directories with ripgrep.")
+
+(defun affe-find-file (&optional dir) (interactive) ; default dir is cwd
+       (setq affe-find-command rg-find-files-command)
+       (affe-find dir))
+(defun affe-find-directory (&optional dir) (interactive) ; default dir is cwd
+       (setq affe-find-command rg-find-directories-command)
+       (affe-find dir))
+(defun affe-find-file-home () (interactive)
+       (affe-find-file (substitute-in-file-name "$HOME")))
+(defun affe-find-directory-home () (interactive)
+       (affe-find-directory (substitute-in-file-name "$HOME")))
+
+(use-package affe
+  :bind (("M-s F"   . affe-find-file-home)
+         ("M-s f" . affe-find-file)
+         ("M-s d"   . affe-find-directory-home))
+  :custom
+  (affe-count 5000))
 
 (use-package which-key
   :config (which-key-mode 1))
