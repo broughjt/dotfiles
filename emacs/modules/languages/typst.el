6344f94d50cef34cf18dfa13d8938398f740319e
Jackson Brough <jacksontbrough@gmail.com>
2026-04-02 20:09:54 -0600

    Set Typst indentation to 2 spaces

--- emacs/modules/languages/typst.el
+++ emacs/modules/languages/typst.el
@@ -6,6 +6,8 @@
 (use-package typst-ts-mode
   :hook
   ((typst-ts-mode . eglot-ensure))
+  :custom
+  (typst-ts-indent-offset 2)
   :config
   (with-eval-after-load 'eglot
     (add-to-list 'eglot-server-programs
