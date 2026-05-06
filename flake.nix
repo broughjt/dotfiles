7d018cfdf44ef6267034e9c5a58b70e292602cbf
Jackson Brough <jacksontbrough@gmail.com>
2026-05-06 17:53:43 -0600

    Add system prompt package

--- flake.nix
+++ flake.nix
@@ -149,6 +149,27 @@
             runHook postInstall
           '';
         };
+
+      piSystemPromptPackage =
+        pkgs:
+        pkgs.stdenvNoCC.mkDerivation rec {
+          pname = "pi-system-prompt";
+          version = "0.1.2";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "jandrikus";
+            repo = "pi-system-prompt";
+            rev = "554623e9c913f866d3bc94d3a2620d26a1feded7";
+            hash = "sha256-zLLF0IlSqoQtSEebEq2t5kInq7mQDjhwUIB5jLwpXyA=";
+          };
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out
+            cp -r package.json README.md LICENSE extensions $out/
+            runHook postInstall
+          '';
+        };
     in
     rec {
       nixosModules = rec {
@@ -756,6 +777,7 @@
           let
             piWebAccess = piWebAccessPackage pkgs;
             piWebMinimal = piWebMinimalPackage pkgs;
+            piSystemPrompt = piSystemPromptPackage pkgs;
           in
           {
             home-manager.users.${config.personal.userName} = {
