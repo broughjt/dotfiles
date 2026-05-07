9ef3d75de8fe40b5a2d021f1ab100afe89761597
Jackson Brough <jacksontbrough@gmail.com>
2026-05-07 01:52:27 -0600

    Nix format on save

--- emacs/modules/languages/nix-config.el
+++ emacs/modules/languages/nix-config.el
@@ -2,10 +2,19 @@
 
 (defvar eglot-server-programs)
 
+(declare-function eglot-format-buffer "eglot")
+
+(defun jackson/nix-format-on-save ()
+  (add-hook 'before-save-hook #'eglot-format-buffer nil t))
+
 (use-package nix-mode
   :mode "\\.nix\\'"
-  :hook (nix-mode . eglot-ensure)
+  :hook ((nix-mode . eglot-ensure)
+         (nix-mode . jackson/nix-format-on-save))
   :config
   (with-eval-after-load 'eglot
     (add-to-list 'eglot-server-programs
-                 '(nix-mode . ("nil")))))
+                 '(nix-mode .
+                   ("nil"
+                    :initializationOptions
+                    (:nil (:formatting (:command ["nixfmt"]))))))))
