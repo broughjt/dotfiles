1cb6a998983e6b29aa189ec6e4a27d7f92ea861d
Jackson Brough <jacksontbrough@gmail.com>
2026-05-07 16:27:34 -0600

    Remove unnecessary vaultix existence gates

--- nix/modules/home/vaultix-secrets.nix
+++ nix/modules/home/vaultix-secrets.nix
@@ -9,9 +9,6 @@ let
   secretsDirectory = ../../../secrets;
   exaApiKeySecret = secretsDirectory + "/exa-api-key.age";
   context7ApiKeySecret = secretsDirectory + "/context7-api-key.age";
-  exaApiKeyAvailable = builtins.pathExists exaApiKeySecret;
-  context7ApiKeyAvailable = builtins.pathExists context7ApiKeySecret;
-  piWebSearchSecretsAvailable = exaApiKeyAvailable || context7ApiKeyAvailable;
 in
 {
   imports = [ vaultixInput.nixosModules.default ];
@@ -22,40 +19,27 @@ in
 
   vaultix.settings.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJaEBK0rIuwE7GqwgeWKA/DvBxIXOcAMDhiORaK9OSf root@murph";
 
-  vaultix.secrets =
-    lib.optionalAttrs exaApiKeyAvailable {
-      exaApiKey.file = exaApiKeySecret;
-    }
-    // lib.optionalAttrs context7ApiKeyAvailable {
-      context7ApiKey.file = context7ApiKeySecret;
-    };
+  vaultix.secrets = {
+    exaApiKey.file = exaApiKeySecret;
+    context7ApiKey.file = context7ApiKeySecret;
+  };
 
-  vaultix.templates = lib.optionalAttrs piWebSearchSecretsAvailable {
-    pi-web-search-json = {
-      name = "pi-web-search.json";
-      owner = user;
-      inherit group;
-      mode = "0600";
-      content = builtins.toJSON (
-        {
-          provider = "auto";
-          workflow = "none";
-          allowBrowserCookies = false;
-        }
-        // lib.optionalAttrs exaApiKeyAvailable {
-          exaApiKey = config.vaultix.placeholder.exaApiKey;
-        }
-        // lib.optionalAttrs context7ApiKeyAvailable {
-          context7ApiKey = config.vaultix.placeholder.context7ApiKey;
-        }
-      );
+  vaultix.templates.pi-web-search-json = {
+    name = "pi-web-search.json";
+    owner = user;
+    inherit group;
+    mode = "0600";
+    content = builtins.toJSON {
+      provider = "auto";
+      workflow = "none";
+      allowBrowserCookies = false;
+      exaApiKey = config.vaultix.placeholder.exaApiKey;
+      context7ApiKey = config.vaultix.placeholder.context7ApiKey;
     };
   };
 
   systemd.tmpfiles.rules = [
     "d ${homeDirectory}/.pi 0700 ${user} ${group} -"
-  ]
-  ++ lib.optionals piWebSearchSecretsAvailable [
     "L+ ${homeDirectory}/.pi/web-search.json - - - - ${config.vaultix.templates.pi-web-search-json.path}"
   ];
 }
