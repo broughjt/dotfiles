fed410d7665925425478e3aab5bcd69104e18711
Jackson Brough <jacksontbrough@gmail.com>
2026-05-07 18:33:20 -0600

    Rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,24 +1,85 @@
 {
+  description = "Rust package";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      crane,
+      fenix,
       flake-utils,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        packageName = cargoToml.package.name;
+
+        toolchain = fenix.packages.${system}.stable.toolchain;
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+        src = craneLib.cleanCargoSource ./.;
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        package = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            meta.mainProgram = packageName;
+          }
+        );
       in
       {
-        devShells.default = pkgs.mkShell {
+        packages = {
+          default = package;
+        }
+        // {
+          ${packageName} = package;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${package}/bin/${packageName}";
+          meta.description = "Run ${packageName}";
+        };
+
+        checks = {
+          default = package;
+
+          clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          fmt = craneLib.cargoFmt { inherit src; };
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
           packages = with pkgs; [
-            cargo
-            clippy
+            cargo-edit
+            cargo-machete
             rust-analyzer
           ];
         };
