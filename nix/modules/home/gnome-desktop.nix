4c8063acd6ca139e8f4c7db734d9b00df184649f
Jackson Brough <jacksontbrough@gmail.com>
2026-05-09 20:31:05 -0600

    Gnome already configures the portals apparently

--- nix/modules/home/gnome-desktop.nix
+++ nix/modules/home/gnome-desktop.nix
@@ -38,10 +38,6 @@
 
     xdg.portal = {
       enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-gnome
-      ];
       config.common.default = [ "gnome" ];
     };
 
