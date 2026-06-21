6ef0f9ed653fcd780a0de9fae5bd2af5d4825ef0
Jackson Brough <jacksontbrough@gmail.com>
2026-06-21 16:56:11 -0600

    Fix typst inlay hint disabling for Weibian

--- emacs/lisp/language-typst.el
+++ emacs/lisp/language-typst.el
@@ -1,18 +1,16 @@
 ;;; -*- lexical-binding: t; -*-
 
 (defvar eglot-server-programs)
-(defvar weibian-directory)
 (declare-function eglot-alternatives "eglot" (servers))
 (declare-function eglot-inlay-hints-mode "eglot" (&optional arg))
 
 (defun jackson/weibian-typst-disable-inlay-hints ()
   "Turn off Eglot inlay hints by default in Weibian Typst buffers."
   (when (and (derived-mode-p 'typst-ts-mode)
-             (bound-and-true-p weibian-directory)
              buffer-file-name
-             (file-in-directory-p
-              (file-truename buffer-file-name)
-              (file-truename (expand-file-name weibian-directory))))
+             (locate-dominating-file
+              (file-name-directory buffer-file-name)
+              "weibian.json"))
     (eglot-inlay-hints-mode -1)))
 
 (use-package typst-ts-mode
