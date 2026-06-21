e3a46105e3416d4b06ee5b3b396fbaa971f03977
Jackson Brough <jacksontbrough@gmail.com>
2026-06-21 14:17:32 -0600

    Turn off inlay hints in Weibian notes by default

--- emacs/lisp/language-typst.el
+++ emacs/lisp/language-typst.el
@@ -1,7 +1,19 @@
 ;;; -*- lexical-binding: t; -*-
 
 (defvar eglot-server-programs)
+(defvar weibian-directory)
 (declare-function eglot-alternatives "eglot" (servers))
+(declare-function eglot-inlay-hints-mode "eglot" (&optional arg))
+
+(defun jackson/weibian-typst-disable-inlay-hints ()
+  "Turn off Eglot inlay hints by default in Weibian Typst buffers."
+  (when (and (derived-mode-p 'typst-ts-mode)
+             (bound-and-true-p weibian-directory)
+             buffer-file-name
+             (file-in-directory-p
+              (file-truename buffer-file-name)
+              (file-truename (expand-file-name weibian-directory))))
+    (eglot-inlay-hints-mode -1)))
 
 (use-package typst-ts-mode
   :mode "\\.typ\\'"
@@ -11,6 +23,8 @@
   (typst-ts-indent-offset 2)
   :config
   (with-eval-after-load 'eglot
+    (add-hook 'eglot-managed-mode-hook
+              #'jackson/weibian-typst-disable-inlay-hints)
     (add-to-list 'eglot-server-programs
                  `((typst-ts-mode) . ,(eglot-alternatives `("tinymist"))))))
 
