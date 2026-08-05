7262c54fec678baa5bedeeef9a226a297212cad4
Jackson Brough <jacksontbrough@gmail.com>
2026-08-05 12:50:01 -0600

    Google

--- nix/modules/home/firefox.nix
+++ nix/modules/home/firefox.nix
@@ -57,8 +57,8 @@ in
           default = "ddg";
           privateDefault = "ddg";
           order = [
-            "ddg"
             "google"
+            "ddg"
           ];
         };
       };
