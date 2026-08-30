817c488d0049827267d8d5dee4aa2d2b45774438
Jackson Brough <jacksontbrough@gmail.com>
2026-08-30 11:06:47 -0600

    Move dconf module out of home modules

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -44,7 +44,7 @@ rec {
   homeDarwin = import ./home/darwin.nix;
   desktopApps = import ./home/desktop-apps.nix { inherit llmAgentsOverlay; };
   gnomeDesktop = import ./home/gnome-desktop.nix { inherit dconf desktopApps; };
-  dconf = import ./home/dconf.nix;
+  dconf = import ./dconf.nix;
   homeGpg = import ./home/gpg.nix;
   homePass = import ./home/pass.nix;
   plaidSync = import ./home/plaid-sync.nix {
