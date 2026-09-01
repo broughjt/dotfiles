d90f44ad9e9a786db4c7a035d64bfe4407f2ed34
Jackson Brough <jacksontbrough@gmail.com>
2026-08-31 19:20:39 -0600

    Bind Ghostty's scrollback dump to ctrl+shift+j

--- nix/modules/home/ghostty.nix
+++ nix/modules/home/ghostty.nix
@@ -6,6 +6,10 @@
     settings = {
       theme = "dark:3024 Night,light:3024 Day";
       font-family = "JuliaMono";
+      # Comes in clutch if you forgot to add `tee`
+      keybind = [
+        "ctrl+shift+j=write_scrollback_file:open"
+      ];
     };
   };
 }
